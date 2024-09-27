4256710cf978b3c476fea079a140bcb4e18e6e49
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-09-27 22:41:10 +0100

    Updated the search input field

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ToggleRegister } from "./contextpage";
 import ProductTemplate from "./extensions/productFunc/productTemplate";
 import { useScroll } from "./usescroll";
 import { useFocus } from "./register_page/main_pages/usefocus";
+import { type } from "@testing-library/user-event/dist/type";
 
 const Search = () => {
 
@@ -10,24 +11,39 @@ const Search = () => {
   const { presentScroll: addScroll } = useScroll("auto", "hidden");
   const { presentScroll: removeScroll } = useScroll("hidden", "auto");
   const { inputFocus } = useFocus(search);
+  const [searchItems, setSearchItems] = useState(products);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    setSearchItems(products.filter((items) => (items.productName).includes(searchInput.toLowerCase())));
+  }, [searchInput]);
 
   return (
     <section ref={addScroll} id="searchSection" className="whiteBackSections">
       <div id="searchHeader" className="navBack"><i ref={removeScroll} onClick={() => setSearch(false)} className="fa-solid fa-xmark navBackIcon"></i></div>
       <main id="searchMain">
         <form id="searchForm">
-          <input ref={inputFocus} type="text" id="searchInput" placeholder="What are you looking for?" />
+          <input ref={inputFocus}
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            type="text"
+            id="searchInput"
+            placeholder="What are you looking for?" />
           <div id="searchIconDiv">
             <i className="fa-solid fa-magnifying-glass" id="searchIcon" type="submit"></i>
           </div>
         </form>
         <div id="searchProductDiv">
-          <h1 className="navHeadText" id="searchHead">Popular Product</h1>
-          <section id="searchProductSection">
-            {products.map((details) => {
-              return <ProductTemplate key={details.id} {...details}></ProductTemplate>
-            })}
-          </section>
+          {searchItems.length > 0 ?
+            <>
+              <h1 className="navHeadText" id="searchHead">Popular Product</h1>
+              <section id="searchProductSection">
+                {searchItems.map((details) => {
+                  return <ProductTemplate key={details.id} {...details}></ProductTemplate>
+                })}
+              </section>
+            </> :
+            <p id="absentText">`{searchInput}` is not available.</p>
+          }
         </div>
       </main>
       <footer id="searchFooter" className="navFooter"></footer>
