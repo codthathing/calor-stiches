472acaa1073417e8388c98613a7c3041e528aba0
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-10-05 23:08:00 +0100

    created product review template

--- src/pages/extensions/products/productreview.jsx
+++ src/pages/extensions/products/productreview.jsx
@@ -3,6 +3,7 @@ import { useParams, useLocation } from "react-router-dom";
 import { productDetails } from "./products";
 import { ToggleRegister } from "../../contextpage";
 import PageLinkTemplate from "../../pagelinks";
+import Handle from "../handleIcons/handle";
 
 const ProductReview = () => {
 
@@ -22,9 +23,9 @@ const ProductReview = () => {
   }, [name, pathname])
 
   const pageLinkDetails = [
-    {id: 0, linkDirect: "/", linkText: "Home", linkArrow: true},
-    {id: 1, linkDirect: "/product", linkText: "Product", linkArrow: true},
-    {id: 2, linkDirect: "", linkText: name, linkArrow: false}
+    { id: 0, linkDirect: "/", linkText: "Home", linkArrow: true },
+    { id: 1, linkDirect: "/product", linkText: "Product", linkArrow: true },
+    { id: 2, linkDirect: "", linkText: name, linkArrow: false }
   ]
 
   return (
@@ -34,6 +35,86 @@ const ProductReview = () => {
           return <PageLinkTemplate {...details}></PageLinkTemplate>
         })}
       </ul>
+      <main id="productReviewMain">
+        <picture id="productReviewImages">
+          <ul id="productSideImages"></ul>
+          <img src="" alt="" id="productMainImage" />
+        </picture>
+        <div id="productDetailsDiv">
+          <p id="productDetailName"></p>
+          <var id="productDetailPrice"></var>
+          <p id="productDetailReview"></p>
+          <p id="productDetailDesc"></p>
+          <div id="productDetailPrep">
+            <p id="productPrepText"></p>
+            <div id="productPrepDiv"></div>
+          </div>
+        </div>
+        <div id="productColor">
+          <p id="productColorText"></p>
+          <main id="productColorMain">
+            <div className="productColorMainDiv"></div>
+            <p className="productColorMainText"></p>
+          </main>
+        </div>
+        <div id="productSize">
+          <div id="productSizeDiv">
+            <p id="productSizeDetail" className="productSizeText"></p>
+            <p id="productSizeChart" className="productSizeText"></p>
+          </div>
+          <main id="productSizeMain">
+            <p className="productSizeMainText"></p>
+          </main>
+        </div>
+        <div id="productFunctions">
+          <div id="productToCart">
+            <div id="productToCartDiv">
+              <div className="productToCartInnerDiv"></div>
+              <div className="productToCartInnerDiv"></div>
+              <div className="productToCartInnerDiv"></div>
+            </div>
+            <button type="button" id="productToCartButton"></button>
+          </div>
+          <div id="productToWishlist">
+            <i id="productWishlistIcon"></i>
+            <p id="productWishlistText"></p>
+          </div>
+        </div>
+        <div id="productDelivery">
+          <div id="productDeliveryInfo">
+            <img src="" alt="" className="preoductDeliveryInfoIcons" />
+            <p className="productDeliveryInfoTexts"></p>
+          </div>
+          <div id="productDeliveryPayment">
+            <p id="productDeliveryPaymentText"></p>
+            <div id="productDeliveryPaymentDiv">
+              <img src="" alt="" className="productDeliveryPaymentImages" />
+            </div>
+          </div>
+          <div id="productDeliveryDetails">
+            <p className="productDeliveryDetailsTexts"></p>
+          </div>
+          <div id="poductHandles">
+            <Handle></Handle>
+          </div>
+        </div>
+        <div id="productAbout">
+          <main id="productAboutMain">
+            <p className="productAboutMainTexts"></p>
+          </main>
+          <p id="productAboutText"></p>
+          <div id="productAboutDiv">
+            <div className="productAboutInnerDiv">
+              <img src="" alt="" className="productAboutInnerImage" />
+              <p className="productAboutInnerText"></p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <div id="productRelatedDvi">
+        <p id="productRelatedText"></p>
+        <main id="productRelatedText"></main>
+      </div>
     </div>
   );
 }
