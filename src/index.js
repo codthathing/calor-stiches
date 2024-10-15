23a8e4cae972cb28c5e985e1e0f1e17278939eec
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-10-15 21:49:29 +0100

    started the product page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "../src/style.css";
 import Home from "./pages/home";
 import { OptNavProvider } from "./pages/contextpage";
 import { BrowserRouter } from "react-router-dom";
 
-
-const Default = () => {
-
-  return (
-    <OptNavProvider>
-      <BrowserRouter>
-        <Home></Home>
-      </BrowserRouter>
-    </OptNavProvider>
-  );
-}
-
-ReactDOM.render(<Default />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <OptNavProvider>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </OptNavProvider>
+);
--- src/pages/extensions/products/productpage.jsx
+++ src/pages/extensions/products/productpage.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ToggleRegister } from "../../contextpage";
 import PageLinkTemplate from "../../pagelinks";
+import ProductTemplate from "../../productFunc/productTemplate";
 
 const ProductPage = () => {
 
-  const { setNavbar, setToggleSideMenu } = useContext(ToggleRegister);
+  const { setNavbar, setToggleSideMenu, products } = useContext(ToggleRegister);
 
   useEffect(() => {
     setNavbar(true);
@@ -13,9 +14,16 @@ const ProductPage = () => {
   }, [])
 
   const pageLinkDetails = [
-    {id: 0, linkDirect: "/", linkText: "Home", linkArrow: true},
-    {id: 1, linkDirect: "", linkText: "Product", linkArrow: false},
+    { id: 0, linkDirect: "/", linkText: "Home", linkArrow: true },
+    { id: 1, linkDirect: "", linkText: "Product", linkArrow: false },
   ]
+  const [mapProducts, setMapProducts] = useState(0);
+  const shownProducts = 2;
+  const pageNumbers = [];
+
+  for (let i = 0; i < Math.floor(products.length / shownProducts); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <div className="otherPages">
@@ -24,6 +32,19 @@ const ProductPage = () => {
           return <PageLinkTemplate {...details}></PageLinkTemplate>
         })}
       </ul>
+      <main>
+        <p>Showing {mapProducts + 1} - {mapProducts + shownProducts} of {products.length} products</p>
+        <section>
+          {products.slice(mapProducts, mapProducts + shownProducts).map((products) => {
+            return <ProductTemplate {...products}></ProductTemplate>
+          })}
+        </section>
+        <div>
+          {pageNumbers.map((pages) => {
+            return <p onClick={() => setMapProducts(shownProducts * pages)}>{pages + 1}</p>
+          })}
+        </div>
+      </main>
     </div>
   );
 }
